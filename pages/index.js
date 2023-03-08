a02faa250636646df5f42cce76c2cb18ad6ff51d
Nakhutsrishvili Nodari <Nakhutsrishvili@yandex.ru>
2023-03-08 15:08:32 +0300

    feat: сайт готов для ревью

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+let nameElement = document.querySelector ('.profile__name');
+let jobElement = document.querySelector ('.profile__about');
+let editButtonElement = document.querySelector ('.profile__edit-button');
+let popupElement = document.querySelector ('.popup');
+let closeButtonElement = document.querySelector ('.popup__close');
+let formElement = document.querySelector ('.form');
+let nameInput = document.querySelector ('.form__input-name');
+let jobInput = document.querySelector ('.form__input-job');
+
+function showPopup() {
+    popupElement.classList.add('popup_opened');
+    nameInput.value = nameElement.textContent;
+    jobInput.value = jobElement.textContent;
+}
+
+function closePopup() {
+    popupElement.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    nameElement.textContent = nameInput.value;
+    jobElement.textContent = jobInput.value;
+    closePopup();
+}
+
+    editButtonElement.addEventListener('click', showPopup);
+    closeButtonElement.addEventListener('click', closePopup);
+    formElement.addEventListener('submit', handleFormSubmit);
+
+
